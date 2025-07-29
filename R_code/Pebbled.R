0ceaa268fb41d505d798357a7b427abc73770310
Emanuel Heitlinger <emanuel.heitlinger@lua.rlp.de>
2025-07-29 11:32:14 +0200

    Cleaning up and doubting thresholds??!

--- R_code/Pebbled.R
+++ R_code/Pebbled.R
@@ -5,6 +5,8 @@ library(xml2)
 library(rentrez)
 library(magrittr)
 library(dplyr)
+library(tidyr)
+library(pheatmap)
 
 sqlfile <- "H:/Analysenetz/transfer/SRAmetadb.sqlite"
 sra_con <- dbConnect(RSQLite::SQLite(), sqlfile)
@@ -34,10 +36,39 @@ tapply(peb$SubjectID, peb$gene,
 tapply(peb$BioSample, peb$gene,
        function(x) length(unique(x)))
 
+all.genes <- as.factor(unique(peb$gene))
 
-table(peb$Title)
+peb <- peb[nchar(peb$BioSample) > 0, ]
 
-table(peb$SubjectID)
+
+peb[peb$gene%in%"VirDb", "gene"] <- substr(as.vector(peb[peb$gene%in%"VirDb", "QueryID"])[[1]], 1, 4)
+
+COpebbled <- peb %>%
+  mutate(presence = TRUE) %>%
+  distinct(BioSample, gene, presence) %>%
+  pivot_wider(
+    names_from = gene,
+    values_from = presence,
+    values_fill = FALSE
+  )
+
+
+
+pheatmap(apply(COpebbled[, -1], 2, as.numeric))
+
+string_peb <- peb[peb$PBSscore>95, ]
+
+sCOpebbled <- string_peb %>%
+  mutate(presence = TRUE) %>%
+  distinct(BioSample, gene, presence) %>%
+  pivot_wider(
+    names_from = gene,
+    values_from = presence,
+    values_fill = FALSE
+  )
+
+pheatmap(apply(sCOpebbled[, -1], 2, as.numeric))
+
 
 ## how many (unique) Samples per Study (proxy: Title)
 ttab <- tapply(peb$BioSample, peb$Title, function(x) length(unique(x)))
@@ -51,6 +82,9 @@ head(ttab)
 
 biosample_ids <- unique(peb$BioSample)
 
+sbiosample_ids <- unique(string_peb$BioSample)
+
+
 ## Starting to work with SRA
 
 dbListTables(sra_con)  # List all tables
@@ -60,7 +94,10 @@ dbListFields(sra_con, "metaInfo")
 
 formatted_ids <- paste0("'", paste(biosample_ids, collapse = "','"), "'")
 
-filtered_query <- sprintf("
+sformatted_ids <- paste0("'", paste(sbiosample_ids, collapse = "','"), "'")
+
+
+study_query <- sprintf("
   SELECT
     study.study_accession AS bioproject,
     study.study_title AS study_title,
@@ -76,64 +113,43 @@ filtered_query <- sprintf("
   HAVING matching_biosample_count > 0
 ", formatted_ids)
 
-
-filtered_result <- dbGetQuery(sra_con, filtered_query)
-
-
-## We lose too many
-sum(filtered_result$matching_biosample_count)
-length(biosample_ids)
-
-
-subject_ids <- unique(peb$SubjectID)
-formatted_srrs <- paste0("'", paste(subject_ids, collapse = "','"), "'")
-
-query_direct <- sprintf("
+sstudy_query <- sprintf("
   SELECT
-    run.run_accession,
-    experiment.experiment_accession,
-    sample.sample_accession AS biosample,
     study.study_accession AS bioproject,
-    study.study_title
-  FROM run
-  JOIN experiment ON run.experiment_accession = experiment.experiment_accession
-  JOIN sample ON experiment.sample_accession = sample.sample_accession
-  JOIN study ON experiment.study_accession = study.study_accession
-  WHERE run.run_accession IN (%s)
-", formatted_srrs)
-
-result <- dbGetQuery(sra_con, query_direct)
-
-
-## ## now compare with the Mgnf (direkt api to json) query
-## table(Mgnf$attributes$accession%in%result$biosample)
-FALSE   TRUE
-433005    870
+    study.study_title AS study_title,
+    study.study_type AS study_type,
+    study.study_abstract as study_abstract,
+    study.study_description as study_description,
+    study.study_attribute as study_attribute,
+    COUNT(DISTINCT CASE WHEN sample.sample_alias IN (%s) THEN sample.sample_alias END) AS matching_biosample_count,
+    COUNT(DISTINCT sample.sample_alias) AS total_biosample_count
+  FROM sample
+  JOIN study ON sample.submission_accession = study.submission_accession
+  GROUP BY study.study_accession, study.study_title
+  HAVING matching_biosample_count > 0
+", sformatted_ids)
 
 
+study_result <- dbGetQuery(sra_con, study_query)
 
-### querrying the subject IDs directly!!!
-head(result)
+sstudy_result <- dbGetQuery(sra_con, sstudy_query)
 
-length(unique(result$biosample))
 
-sum(filtered_result$matching_biosample_count)
-
+## We lose many
+sum(study_result$matching_biosample_count)
+length(biosample_ids)
 length(unique(peb$BioSample))
 
-filtered_result$short_title <- substr(filtered_result$study_title, 1, 40)
-filtered_result$prevalence_raw <- filtered_result$matching_biosample_count/
-    filtered_result$total_biosample_count
+study_result$short_title <- substr(study_result$study_title, 1, 40)
+study_result$prevalence_raw <- study_result$matching_biosample_count/
+    study_result$total_biosample_count
 
-## filtered_result[ , !colnames(filtered_result)%in%c("study_title",
-##                                                    "study_abstract")]
+study_result$twin_study <- grepl("twin", study_result$study_abstract, ignore.case=TRUE)
 
-filtered_result$twin_study <- grepl("twin", filtered_result$study_abstract, ignore.case=TRUE)
-
-filtered_result$healthy_study <- grepl("healthy", filtered_result$study_abstract,
+study_result$healthy_study <- grepl("healthy", study_result$study_abstract,
                                        ignore.case=TRUE)
 
-filtered_result <- filtered_result %>%
+study_result <- study_result %>%
     mutate(species_study = case_when(
                grepl("patient|adult|human|twin|child|infant|participant",
                      study_abstract, ignore.case = TRUE) ~ "human",
@@ -142,26 +158,9 @@ filtered_result <- filtered_result %>%
                TRUE ~ "unknown"  # Default case
            ))
 
+sstudy_result$prevalence_raw <- sstudy_result$matching_biosample_count/
+  sstudy_result$total_biosample_count
 
-table(filtered_result$study_type, filtered_result$species_study)
-
-## ## now compare with the Mgnf (direkt api to json) query
-table(Mgnf$attributes$accession%in%)
-
-
-
-filtered_result_with_labels <- filtered_result[filtered_result$total_biosample_count > 250, ]
-
-ggplot(filtered_result, aes(y = prevalence_raw, x = species_study,
-                            color = twin_study,
-                            shape = study_type,
-                            size = total_biosample_count)) +
-    geom_jitter() ## +
-    ## geom_text(data = filtered_result_with_labels,
-    ##           aes(label = short_title),
-    ##           vjust = -0.5,  # Adjust vertical position
-    ##           hjust = 1,     # Adjust horizontal position
-    ##           check_overlap = TRUE)  # Avoid overlapping text
 
 
 
@@ -174,7 +173,7 @@ ggplot(filtered_result, aes(y = prevalence_raw, x = species_study,
 ## Many samples are missing information or have incomplete information
 ## we also seem to lose samples
 
-detailed_query <- sprintf("
+sample_query <- sprintf("
   SELECT
     sample.sample_ID,
     sample.sample_alias,
@@ -188,7 +187,7 @@ detailed_query <- sprintf("
 ", formatted_ids)
 
 
-detailed_query <- sprintf("
+sample_query <- sprintf("
   SELECT
     sample.sample_ID,
     sample.sample_alias,
@@ -206,99 +205,94 @@ detailed_query <- sprintf("
 ", formatted_ids)
 
 
-detailed_result <- dbGetQuery(sra_con, detailed_query)
-head(detailed_result)
-nrow(detailed_result)
+sample_result <- dbGetQuery(sra_con, sample_query)
+head(sample_result)
+nrow(sample_result)
 
-table(biosample_ids%in%detailed_result$sample_alias, useNA="ifany")
+table(biosample_ids%in%sample_result$sample_alias, useNA="ifany")
 
-missingIDs <- biosample_ids[!biosample_ids%in%detailed_result$sample_alias]
+sum(study_result$matching_biosample_count)
 
-f_missing_ids <- paste0("'", paste(missingIDs, collapse = "','"), "'")
+table(sample_result$study_acc%in%study_result$bioproject, useNA="ifany")
+table(study_result$bioproject%in%sample_result$study_acc, useNA="ifany")
+length(unique(sample_result$study_acc))
+### ... okay this fits 165 studies!
 
-missing_query <- sprintf("
-  SELECT
-    sample.sample_ID,
-    sample.sample_alias,
-    sample.sample_accession,
-    sample.taxon_id,
-    study.study_ID AS study_ID,
-    study.study_accession AS study_acc,
-    study.study_alias AS study_ali,
-    study.submission_accession AS study_sub_acc,
-    sample.submission_accession AS sample_sub_acc
-  FROM sample
-  LEFT OUTER JOIN study
-    ON sample.submission_accession = study.submission_accession
-  WHERE sample.sample_alias IN (%s)
-", f_missing_ids)
+## have a manual look!!
+study_result[order(study_result$prevalence_raw, decreasing=TRUE),
+             c("total_biosample_count", "short_title")]
 
 
-## still missing!
-## head(missing_result)
+write.csv2(study_result, file="manual_pebble.csv")
+
+study_result <- read.csv2(file="manual_pebble_FINISHED.csv")
 
 
-missing_query <- sprintf("
-  SELECT
-    sample.sample_ID,
-    sample.sample_alias,
-    sample.sample_accession,
-    study.study_ID,
-    study.study_accession
-  FROM sample
-  LEFT JOIN study ON sample.submission_accession = study.submission_accession
-  WHERE sample.sample_alias IN (%s) OR sample.sample_accession IN (%s)
-", f_missing_ids, f_missing_ids)
-
-missing_result <- dbGetQuery(sra_con, missing_query)
-
-## still missing
-head(missing_result)
-
-
-dbListTables(sra_con)  # List all tables
-dbListFields(sra_con, "study")  # Check columns in the study table
-dbListFields(sra_con, "sample")  # Check columns in the sample table
-dbListFields(sra_con, "experiment")  # Check columns in the experiment table
+write.csv2(study_result, file="manual_pebble.csv")
 
 
 
-Entrez_list <- lapply(missingIDs[1:8], function(ID) {
-    result <- entrez_search(db = "biosample", term = ID)
-    entrez_summary(db = "biosample", id = result$ids)
-})
+table(study_result$study_type, study_result$verdict)
 
 
-parse_biosample <- function(sample_data) {
-    parsed_xml <- read_xml(sample_data)
-    tibble(
-        biosample_id = xml_text(xml_find_first(parsed_xml, "//Id[@db='BioSample']")),
-        sample_name = xml_text(xml_find_first(parsed_xml, "//Id[@db_label='Sample name']")),
-        sra_id = xml_text(xml_find_first(parsed_xml, "//Id[@db='SRA']")),
-        title = xml_text(xml_find_first(parsed_xml, "//Description/Title")),
-        taxonomy_id = xml_attr(xml_find_first(parsed_xml, "//Organism"), "taxonomy_id"),
-        taxonomy_name = xml_attr(xml_find_first(parsed_xml, "//Organism"), "taxonomy_name"),
-        owner = xml_text(xml_find_first(parsed_xml, "//Owner/Name")),
-        first_name = xml_text(xml_find_first(parsed_xml, "//Contacts/Contact/Name/First")),
-        last_name = xml_text(xml_find_first(parsed_xml, "//Contacts/Contact/Name/Last")),
-        model = xml_text(xml_find_first(parsed_xml, "//Models/Model")),
-        package = xml_text(xml_find_first(parsed_xml, "//Package")),
-        host = xml_text(xml_find_first(parsed_xml, "//Attribute[@attribute_name='host']")),
-        isolation_source = xml_text(xml_find_first(parsed_xml, "//Attribute[@attribute_name='isolation_source']")),
-        collection_date = xml_text(xml_find_first(parsed_xml, "//Attribute[@attribute_name='collection_date']")),
-        geo_loc_name = xml_text(xml_find_first(parsed_xml, "//Attribute[@attribute_name='geo_loc_name']")),
-        lat_lon = xml_text(xml_find_first(parsed_xml, "//Attribute[@attribute_name='lat_lon']")),
-        sample_processing = xml_text(xml_find_first(parsed_xml, "//Attribute[@attribute_name='samp_mat_process']")),
-        sample_size = xml_text(xml_find_first(parsed_xml, "//Attribute[@attribute_name='samp_size']")),
-        bioproject_id = xml_text(xml_find_first(parsed_xml, "//Link[@type='entrez' and @target='bioproject']"))
-    )
-}
+study_result %>%
+  filter(grepl("Good", study_result$verdict)) %>%
+  ggplot(aes(x=total_biosample_count, y = prevalence_raw,
+                         size = total_biosample_count,
+             color = verdict)) +
+  geom_point() +
+  scale_y_continuous("raw prevalence %", breaks = c(1:4/10),
+                     labels = c(1:4*10)) +
+  scale_x_continuous("total numbe of samples in study")
 
-## Apply the function to all samples
-all_biosamples <- lapply(Entrez_list, parse_biosample)
 
-## Combine the individual tibbles into a single table
-biosample_table <- bind_rows(all_biosamples)
 
-head(biosample_table)
+### below I try to find the missing IDs without success... commented in the
+## interest of running this here faster.
+
+# missingIDs <- biosample_ids[!biosample_ids%in%sample_result$sample_alias]
+#
+# f_missing_ids <- paste0("'", paste(missingIDs, collapse = "','"), "'")
+#
+# missing_query <- sprintf("
+#   SELECT
+#     sample.sample_ID,
+#     sample.sample_alias,
+#     sample.sample_accession,
+#     sample.taxon_id,
+#     study.study_ID AS study_ID,
+#     study.study_accession AS study_acc,
+#     study.study_alias AS study_ali,
+#     study.submission_accession AS study_sub_acc,
+#     sample.submission_accession AS sample_sub_acc
+#   FROM sample
+#   LEFT OUTER JOIN study
+#     ON sample.submission_accession = study.submission_accession
+#   WHERE sample.sample_alias IN (%s)
+# ", f_missing_ids)
+
+#
+# missing_result <- dbGetQuery(sra_con, missing_query)
+#
+#
+# ## still missing
+# head(missing_result)
+
+# missing_query <- sprintf("
+#   SELECT
+#     sample.sample_ID,
+#     sample.sample_alias,
+#     sample.sample_accession,
+#     study.study_ID,
+#     study.study_accession
+#   FROM sample
+#   LEFT JOIN study ON sample.submission_accession = study.submission_accession
+#   WHERE sample.sample_alias IN (%s) OR sample.sample_accession IN (%s)
+# ", f_missing_ids, f_missing_ids)
+#
+# missing_result <- dbGetQuery(sra_con, missing_query)
+#
+# ## still missing
+# head(missing_result)
+
 
