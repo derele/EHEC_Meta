55408dd2be01336c4318751a3080f1f830b4c783
Emanuel Heitlinger <emanuel.heitlinger@lua.rlp.de>
2025-08-19 13:01:16 +0200

    false negative estimation via Loman2013

--- R_code/curatedMetagenomicData.R
+++ R_code/curatedMetagenomicData.R
@@ -3,6 +3,9 @@ library(dplyr)
 library(magrittr)
 library(tidyverse)
 library(pheatmap)
+library(ggplot2)
+library(boot)
+
 
 
 write_NEW_SRA <- FALSE
@@ -126,29 +129,187 @@ Screen_df <- as.data.frame(Screen)
 
 rownames(Screen_df) <- Screen_df$sample
 
-specificRS <- rowSums(Screen[, grepl("bt_|hits_95_", colnames(Screen))])
-
-stx2RS <- rowSums(Screen[, grepl("stx2", colnames(Screen))])
-
-pheatmap(log10(Screen_df[stx2RS>1, grepl("stx2", colnames(Screen_df))]+1),
-        )
-
-Stx2_samples <- rownames(Screen_df[stx2RS>1,])
-
+## need to work on the controls!!
 controls <- c("SRR12195337", "SRR12195338", "SRR12195339",
               "SRR4101313", "SRR4101314", "SRR4101315", "SRR33438544",
               "SRR32314265")
 
-length(Stx2_samples)/nrow(Screen)*100
-
-sM[sM$NCBI_accession%in%Stx2_samples&
-  !sM$NCBI_accession%in%controls   , 1:25]
-
-
 ### need to expand the NCBI_ID here once mutliple runs per samples are read!
+finalData <- inner_join(sM, Screen, by = c("NCBI_accession" = "sample")) %>%
+  as.data.frame()
 
-finalData <- inner_join(sM, Screen, by = c("NCBI_accession" = "sample"))
+finalData$my_sample_ID <- make.unique(finalData$NCBI_accession)
 
-foo <- finalData[finalData$NCBI_accession%in%Stx2_samples, ]
+rownames(finalData) <- finalData$my_sample_ID
+
+screenCols <- colnames(finalData[, grepl("bt_|hits_", colnames(finalData))])
+specificCols <- colnames(finalData[, grepl("bt_|hits_95_", colnames(finalData))])
+stxCols <- colnames(Screen[, grepl("stx", colnames(Screen))])
+stx2Cols <- colnames(Screen[, grepl("stx2", colnames(Screen))])
+
+### back to the contreols in the overall screening dataset... to see some
+## non-human (non curatedMetagenomcData merging)
+pheatmap(log10(Screen_df[controls ,screenCols]+1))
 
 
+posStudies <- rownames(finalData)[rowSums(finalData[, screenCols])>5]
+pposStudies <- rownames(finalData)[rowSums(finalData[, specificCols])>5]
+stxStudies <- rownames(finalData)[rowSums(finalData[, stxCols])>3]
+stx2Studies <- rownames(finalData)[rowSums(finalData[, stx2Cols])>0]
+
+finalData$DE2011_outbreak <- ifelse(finalData$study_name=="LomanNJ_2013",
+                                    "Yes", "No")
+
+study_annot <- finalData[, c("DE2011_outbreak", "disease", "age", "study_name")]
+study_annot$age <- as.numeric(study_annot$age)
+
+pheatmap(log10(finalData[posStudies, screenCols]+1),
+         annotation_row = study_annot[, c(1, 3)],
+         show_rownames = FALSE)
+
+## based on this (and controls we should drop eae < 95%)
+screenCols <- screenCols[!screenCols%in%c("hits_VFG000739_eae", "hits_VFG000803_eae")]
+
+pheatmap(log10(finalData[pposStudies, screenCols]+1),
+         annotation_row = study_annot[, c(1, 3)],
+         show_rownames = FALSE)
+
+pheatmap(log10(finalData[stxStudies, screenCols]+1),
+         annotation_row = study_annot[, c(1, 2, 3)],
+         show_rownames = FALSE)
+
+pheatmap(log10(finalData[stx2Studies, screenCols]+1),
+         annotation_row = study_annot[, c(2,3)],
+         show_rownames = FALSE)
+
+
+pheatmap(log10(finalData[finalData$disease%in%"STEC", screenCols]+1),
+        ## annotation_row = study_annot[, c(2,3)],
+         show_rownames = FALSE)
+
+
+
+length(Stx2_samples)/nrowposStudieslength(Stx2_samples)/nrow(finalData)*100
+
+tapply(finalData$number_bases, finalData$study_name, mean)/1000000
+
+table(finalData[!finalData$study_name%in%"LomanNJ_2013", "bt_VFG000837_stx2A"])
+table(finalData[!finalData$study_name%in%"LomanNJ_2013",
+                "bt_VFG000837_stx2A"]>0)
+
+finalData$STX2bt <- finalData$bt_VFG000837_stx2A>0|
+  finalData$bt_VFG000838_stx2B>0
+
+nrow(finalData[finalData$STX2bt & !finalData$study_name%in%"LomanNJ_2013", ])/
+  nrow(finalData[!finalData$study_name%in%"LomanNJ_2013", ])*100
+### 0.3624883%
+
+## but detection is dependent on sequencing depth
+depth_model<- glm(STX2bt ~ number_bases,
+                 data=finalData[finalData$disease%in%"healthy",],
+                 family = binomial)
+
+## From LomanNJ_2013 depth at their 0.67% sensitivity
+# 1. Get reference prediction
+ref_depth <- data.frame(number_bases = 1.075e9)
+current_prob <- predict(depth_model, newdata = ref_depth, type = "response")
+
+target_prob <- 0.67
+current_linear_pred <- predict(depth_model, newdata = ref_depth, type = "link")  # Log-odds
+beta_1 <- coef(depth_model)["number_bases"]
+new_intercept <- log(target_prob / (1 - target_prob)) - beta_1 * ref_depth$number_bases
+
+depth_model_adjusted <- depth_model
+depth_model_adjusted$coefficients["(Intercept)"] <- new_intercept
+
+predict(depth_model_adjusted, newdata = ref_depth, type = "response")  # Should output 0.67
+
+new_data <- data.frame(number_bases = seq(1e8, 4e10, length.out = 100))
+new_data$prob <- predict(depth_model_adjusted, newdata = new_data, type = "response")
+
+
+
+finalData[finalData$disease%in%"healthy", "pred_sensitivity"] <-
+  predict(depth_model_adjusted,
+          new_data = finalData[finalData$disease%in%"healthy","number_bases"],
+          type="response") + 0.67
+
+
+# 4. Plot
+ggplot(new_data, aes(x = number_bases, y = prob)) +
+  geom_line() +
+  geom_vline(xintercept = 1.075e9, linetype = "dashed", color = "red") +
+  geom_hline(yintercept = 0.67, linetype = "dashed", color = "red") +
+  # geom_point(data=finalData, aes(x = number_bases, y = pred_sensitivity,
+  #                                color=study_name)) +
+  labs(x = "Sequencing Depth (number_bases)", y = "Adjusted Detection Probability",
+       title = "Logistic Model Calibrated to 67% Sensitivity at 1.075B Reads")
+
+
+
+ggsave("figures/detection_depht.png")
+
+
+
+## At about 4e+10 = 40,000,000,000 == 40 GB we can expect to have 100% sensitivity.
+
+table(finalData$number_bases > 4e+10) ## no 100% sensitivity
+table(finalData$number_bases > 1.3e+10) ## 160 samples with 95% sensitivity at 13GB data
+
+table(finalData[!finalData$study_name%in%"LomanNJ_2013", "STX2bt"])
+
+finalData$pred_sensitivity <- predict(depth_model_adjusted,
+                                      newdata = finalData,
+                                      type = "response")
+
+true_positives <- sum(finalData$STX2bt / finalData$pred_sensitivity)
+
+true_positives/nrow(finalData)*100
+
+set.seed(123)
+boot_fn <- function(data, indices) {
+  sample_data <- data[indices, ]
+  obs_pos <- sum(sample_data$STX2bt)
+  pred_sens <- predict(depth_model_adjusted, newdata = sample_data, type = "response")
+  true_pos <- sum(sample_data$STX2bt / pred_sens)
+  true_prev <- true_pos / nrow(sample_data)
+  return(true_prev)
+}
+boot_results <- boot(finalData, boot_fn, R = 1000)
+quantile(boot_results$t, c(0.025, 0.975))  # 95% CI
+
+boot_df <- data.frame(true_prevalence = boot_results$t)
+
+ggplot(boot_df, aes(x = true_prevalence)) +
+  geom_histogram(aes(y = ..density..),
+                 bins = 30,
+                 fill = "skyblue",
+                 color = "black",
+                 alpha = 0.7) +
+  geom_density(linewidth = 1, color = "darkblue") +
+  geom_vline(xintercept = mean(boot_results$t),
+             linetype = "solid",
+             color = "red",
+             linewidth = 1) +
+  geom_vline(xintercept = quantile(boot_results$t, c(0.025, 0.975)),
+             linetype = "dashed",
+             color = "red",
+             linewidth = 0.8) +
+  labs(
+    x = "Prevalence of stx2AB",
+    y = "Density",
+    title = "Bootstrap Distribution of Prevalence Estimates",
+    subtitle = "Red lines: Mean (solid) and 95% CI (dashed)"
+  ) +
+  theme_minimal()
+
+
+ggsave("figures/bootstrap_estimate.png")
+
+
+disease_model<- glm(STX2bt ~ disease,
+                    ##                  data=finalData[!finalData$study_name%in%"LomanNJ_2013",],
+                    data=finalData,
+                    family = binomial)
+
+summary(disease_model)
